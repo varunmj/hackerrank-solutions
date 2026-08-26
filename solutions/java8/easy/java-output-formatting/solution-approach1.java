e905e252cddbdd5e016a4cedbf1195878e0f4087
Varun Mohankumar Jayasree <varunmj@users.noreply.github.com>
2026-08-26 16:34:08 -0500

    ✅ Java Output Formatting [java8] — approach1

---
+++ solutions/java8/easy/java-output-formatting/solution-approach1.java
@@ -0,0 +1,33 @@
+// ──────────────────────────────────────────────────
+// Link        https://www.hackerrank.com/challenges/java-output-formatting/problem?isFullScreen=true
+// Problem     Java Output Formatting
+// Difficulty  Easy
+// Subdomain   Introduction
+// Platform    HackerRank
+// Language    java8
+// Status      Accepted
+// Submitted   2026-08-26, 05:34 p.m.
+// ──────────────────────────────────────────────────
+
+import java.util.Scanner;
+
+public class Solution {
+
+    public static void main(String[] args) {
+            Scanner sc=new Scanner(System.in);
+            System.out.println("================================");
+            for(int i=0;i<3;i++)
+            {
+                String s1=sc.next();
+                int x=sc.nextInt();
+                //Complete this line
+                
+                System.out.printf("%-15s%03d%n", s1, x);
+            }
+            System.out.println("================================");
+
+    }
+}
+
+
+
